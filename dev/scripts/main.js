cc695f26dd827e051d6affd3eede324e0ae9df7f
Caroline Pisano <pisanocaroline@gmail.com>
2018-06-04 15:45:42 -0400

    added hamburger menu and fixed some mobile issues

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,4 +1,21 @@
 $(function(){
+    (function () {
+
+        var hamburger = {
+            navToggle: document.querySelector('.navToggle'),
+            nav: document.querySelector('nav'),
+
+            doToggle: function (e) {
+                e.preventDefault();
+                this.navToggle.classList.toggle('expanded');
+                this.nav.classList.toggle('expanded');
+            }
+        };
+
+        hamburger.navToggle.addEventListener('click', function (e) { hamburger.doToggle(e); });
+
+    }());
+
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
 
